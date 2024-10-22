56ffe8ca2549c7139534734d858762f290174e19
JayJai04 <jayanth.jaisankar@gmail.com>
2024-10-21 17:04:48 -0700

    Use this version to avoid deprecated packages

--- Quantized_LLM.py
+++ Quantized_LLM.py
@@ -1,5 +1,4 @@
-from langchain_community.llms import Ollama
-model = Ollama(model = 'llama2')
+from langchain_ollama import OllamaLLM
+model = OllamaLLM(model = 'llama2')
 prompt = 'who are you?'
 print(model.invoke(prompt))
-
